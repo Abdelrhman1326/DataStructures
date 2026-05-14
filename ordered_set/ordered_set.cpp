a6f3ce8d9c1c29bdf7b4ab3d0e4f537bbac0b0fb
AbdElrhman Mohamed <abdelrhman132006@gmail.com>
2026-05-14 17:24:34 +0300

    ordered_set template edited

--- ordered_set/ordered_set.cpp
+++ ordered_set/ordered_set.cpp
@@ -5,6 +5,5 @@ using namespace std;
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-// insert/delete in O(log n)
-// still query by index
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
